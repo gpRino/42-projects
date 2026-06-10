fe450b4389cbff7d451a4d584bd02608855d7e59
Gianmarco Leccia <gleccia@c2r3s6.42roma.it>
2026-06-10 15:14:34 +0200

    ready to make

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libeft.h"
+#include "libft.h"
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libeft.h"
+#include "libft.h"
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
