67ac432a9cd2189e19fafdcb23a065090edc63be
gpRino <gianmarco.leccia@gmail.com>
2026-07-02 10:44:55 +0200

    updated first ex, no norm no cc

--- Exam_02/Level_1/first_word/first_word.c
+++ Exam_02/Level_1/first_word/first_word.c
@@ -1,4 +1,15 @@
+#include <unistd.h>
+
 int main(int argc, char ** argv)
 {
-    
+    int i;
+
+    i = 0;
+    if (argc != 2)
+        return (0);
+    while (argv[1][i])
+    {
+        write(1, &argv[1][i], 1);
+        i++;
+    }
 }
