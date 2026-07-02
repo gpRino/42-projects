25da63919acf37e2d6422594923110356bb57952
gpRino <gianmarco.leccia@gmail.com>
2026-07-02 14:05:17 +0200

    svolto e funzionante, main commentato

---
+++ Exam_02/Level_1/ft_strcpy/ft_strcpy.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+char    *ft_strcpy(char *s1, char *s2)
+{
+    int i;
+
+    i = 0;
+    while (s2[i])
+    {
+        s1[i] = s2[i];
+        i++;
+    }
+    s1[i] = '\0';
+    return (s1);
+}
+
+/*int main(void)
+{
+    char s1[100] = "12345";
+    char s2[] = "123";
+
+    ft_strcpy(s1, s2);
+    write(1, s1, 5);
+    write(1, "\n", 1);
+    return (0);
+}*/
