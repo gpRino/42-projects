72760652e10755757792b648e1da53385e87fda5
gpRino <gianmarco.leccia@gmail.com>
2026-07-13 15:40:14 +0200

    lvl 1 completed

--- Exam_02/Level_1/repeat_alpha/repeat_alpha.c
+++ Exam_02/Level_1/repeat_alpha/repeat_alpha.c
@@ -5,38 +5,21 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gleccia <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-<<<<<<< HEAD
 /*   Created: 2026/07/13 13:41:23 by gleccia           #+#    #+#             */
 /*   Updated: 2026/07/13 13:41:23 by gleccia          ###   ########.fr       */
-=======
-/*   Created: 2026/07/12 10:25:59 by gleccia           #+#    #+#             */
-/*   Updated: 2026/07/12 10:53:51 by gleccia          ###   ########.fr       */
->>>>>>> 7b072c26025f4a447c2f4ff9a20d0fabdbcd2bc8
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-<<<<<<< HEAD
 void ft_putchar(char c)
 {
     write(1, &c, 1);
-=======
-void ft_putchar(char c, int i)
-{
-    while (i > 0)
-    {
-        write(1, &c, 1);
-        i--;
-    }
-
->>>>>>> 7b072c26025f4a447c2f4ff9a20d0fabdbcd2bc8
 }
 
 void repeat_alpha(char *str)
 {
     int i;
-<<<<<<< HEAD
     int j;
 
     j = 0;
@@ -57,31 +40,11 @@ void repeat_alpha(char *str)
         }
        }
        i++;
-=======
-
-    i = 0;
-    while (str[i])
-    {
-        if (str[i] >= 'A' && str[i] <= 'Z')
-        {
-            ft_putchar(str[i], str[i] + 1 - 'A');
-        }
-        else if (str[i] >= 'a' && str[i] <= 'z')
-        {
-            ft_putchar(str[i], str[i] + 1 - 'a');
-        }
-        else
-        {
-            write(1, &str[i], 1);
-        }
-        i++;
->>>>>>> 7b072c26025f4a447c2f4ff9a20d0fabdbcd2bc8
     }
 }
 
 int main (int argc, char **argv)
 {
-<<<<<<< HEAD
     if (argc != 2)
     {
         write(1, "\n", 1);
@@ -89,12 +52,4 @@ int main (int argc, char **argv)
     }
     repeat_alpha(argv[1]);
     return(0);
-=======
-    if (argc == 2)
-    {
-        repeat_alpha(argv[1]);
-    }
-    write(1, "\n", 1);
-    return (0);
->>>>>>> 7b072c26025f4a447c2f4ff9a20d0fabdbcd2bc8
 }
--- Exam_02/Level_1/rev_print/rev_print.c
+++ Exam_02/Level_1/rev_print/rev_print.c
@@ -5,28 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gleccia <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-<<<<<<< HEAD
 /*   Created: 2026/07/13 14:10:28 by gleccia           #+#    #+#             */
 /*   Updated: 2026/07/13 14:10:28 by gleccia          ###   ########.fr       */
-=======
-/*   Created: 2026/07/12 10:57:29 by gleccia           #+#    #+#             */
-/*   Updated: 2026/07/12 11:04:32 by gleccia          ###   ########.fr       */
->>>>>>> 7b072c26025f4a447c2f4ff9a20d0fabdbcd2bc8
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-<<<<<<< HEAD
 int main(int argc, char **argv)
-=======
-int ft_strlen(char *str)
->>>>>>> 7b072c26025f4a447c2f4ff9a20d0fabdbcd2bc8
 {
     int i;
 
     i = 0;
-<<<<<<< HEAD
     if (argc != 2)
     {
         write (1, "\n", 1);
@@ -42,31 +32,4 @@ int ft_strlen(char *str)
         write (1, &argv[1][i], 1);
         i--;
     }
-=======
-    while (str[i] != '\0')
-        i++;
-    return (i);
-}
-
-void rev_print(char *str)
-{
-    int i;
-
-    i = ft_strlen(str) - 1;
-    while (i >= 0)
-    {
-        write(1, &str[i], 1);
-        i--;
-    }
-}
-
-int main (int argc, char **argv)
-{
-    if (argc == 2)
-    {
-        rev_print(argv[1]);
-    }
-    write(1, "\n", 1);
-    return (0);
->>>>>>> 7b072c26025f4a447c2f4ff9a20d0fabdbcd2bc8
 }
